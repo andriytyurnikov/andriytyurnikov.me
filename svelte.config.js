2924efbccf0e0948735bcf301ba05baefc14b4a9
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-09 01:45:18 +0200

    :zap: tailwindcss setup

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess({})],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -14,7 +13,8 @@ const config = {
 				'connect-src': ['self', 'https://*.vercel-insights.com', 'https://*.vercel-analytics.com']
 			}
 		}
-	}
+	},
+	preprocess: [vitePreprocess({})]
 };
 
 export default config;
