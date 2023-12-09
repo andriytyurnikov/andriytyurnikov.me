8122eb72f98fd582ac1c0e192fa1d74b69700fdf
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2023-12-09 08:45:20 +0200

    :zap: -webkit-fill-available fixed

--- src/routes/(tokenmaker)/tokenmaker/+page.svelte
+++ src/routes/(tokenmaker)/tokenmaker/+page.svelte
@@ -3,21 +3,22 @@
 </script>
 
 <article class="p-4 scena-latus mx-auto">
-  <h1 class="text-center my-8">
+  <h2 class="text-center my-8">
     TokenMaker
-  </h1>
+  </h2>
   <hr class="my-8 border-current"/>
   <h4 class="mb-10/6r">
     Mint your tokens safely on 
-    <span class="text-[#a6a6ad] font-bold">Ethereum</span>,
-    <span class="text-[#7b3fe4] font-bold">Polygon</span>,
-    <span class="text-[#0d8e74] font-bold">Gnosis</span>,
     <span class="text-[#e84142] font-bold">Avalanche</span>,
-    
+    <span class="text-[#3b82f6] font-bold">Base</span>,
+    <span class="text-[#f0b90b] font-bold">BSC</span>,
+    <span class="text-[#3b3b3b] dark:text-[#a6a6ad] font-bold">Ethereum</span>,
+    <span class="text-[#0d8e74] font-bold">Gnosis</span>,
+    <span class="text-[#7b3fe4] font-bold">Polygon</span>
     <!-- <span class="text-[#888888]">Arbitrum</span>,
     <span class="text-[#888888]">Aurora</span>, -->
-    <span class="text-[#3b82f6] font-bold">Base</span>,
-    <span class="text-[#f0b90b] font-bold">BSC</span>
+    
+    
     
     <!-- <span class="text-[#888888]">Celo</span>,
     <span class="text-[#888888]">Fantom</span>,
