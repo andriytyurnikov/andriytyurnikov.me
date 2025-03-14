8cd4f75e39372f945548a56779db79b4afa5fa44
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2025-03-14 02:16:07 +0200

    responsive FOV

---
+++ src/routes/(unstyled)/garage/fov/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import '../../../../styles/unstyled.css';
+
+	import { base } from '$app/paths';
+</script>
+
+<article
+	class="grayscale saturate-80 overflow-clip bg-viridis-950 h-dvh flex flex-col flex-1 grow place-items-center place-content-center"
+>
+	<div
+		class="rounded-[50%] bg-radial from-viridis-500 to-viridis-900 w-[60rem] h-[48rem] flex place-items-center place-content-center"
+	>
+		<div
+			class="rounded-[50%] bg-radial from-viridis-300 to-viridis-500 w-[40rem] h-[32rem] flex place-items-center place-content-center"
+		>
+			<div
+				class="rounded-[50%] bg-radial from-viridis-100 to-viridis-300 w-[32rem] h-[24rem] flex place-items-center place-content-center"
+			>
+				<div
+					class="rounded-[50%] bg-radial from-viridis-50 to-viridis-100 w-[24rem] h-[20rem] flex place-items-center place-content-center"
+				>
+					<div
+						class="rounded-[50%] bg-radial from-viridis-50 to-viridis-100/50 w-[20rem] h-[16rem] flex"
+					></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
