13c5445ce6e859d67458ae54237642c64299536b
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2025-04-02 00:26:48 +0300

    home page done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,10 +27,10 @@
 			</div>
 
 			<p class="mt-8 text-[1rem] leading-12 text-right">
-				<a class="u-email text-white" href="mailto:Andriy.Tyurnikov@gmail.com"
+				<a class="u-email text-white decoration-dashed" href="mailto:Andriy.Tyurnikov@gmail.com"
 					>Andriy.Tyurnikov@gmail.com</a
 				>
-				<span class="inline-flex p-2 bg-neutral-800 rounded-sm">
+				<!-- <span class="inline-flex p-2 bg-neutral-800 rounded-sm">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -45,10 +45,10 @@
 							d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
 						/>
 					</svg>
-				</span>
+				</span> -->
 				<br />
 				<a class="u-tel text-white" href="tel:+380-67-106-10-26">+380-67-106-10-26</a>
-				<span class="inline-flex p-2 bg-neutral-800 rounded-sm">
+				<!-- <span class="inline-flex p-2 bg-neutral-800 rounded-sm">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -63,7 +63,7 @@
 							d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
 						/>
 					</svg>
-				</span>
+				</span> -->
 				<br />
 			</p>
 		</div>
@@ -145,5 +145,9 @@
 	:global(html) {
 		background-color: theme(--color-blue-800);
 		color: theme(--color-white);
+	}
+
+	a {
+		@apply decoration-dashed decoration-1 underline-offset-4;
 	}
 </style>
